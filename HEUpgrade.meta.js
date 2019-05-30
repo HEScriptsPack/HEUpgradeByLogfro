64fdd15ae72d6e1e36acf9a4117778e8eff7371a
Logfro <admin@logfro.de>
2019-05-30 09:08:44 +0200

    Added auto-update

---
+++ HEUpgrade.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         HEUpgrade Rework
+// @namespace    https://logfro.de/
+// @version      0.1
+// @description  Upgrading all things which are added to the Que
+// @author       You
+// @match        https://legacy.hackerexperience.com/hardware*
+// @updateURL    https://gitcdn.xyz/repo/Logfro/HEUpgrade/master/HEUpgrade.meta.js
+// @downloadURL  https://gitcdn.xyz/repo/Logfro/HEUpgrade/master/HEUpgrade.user.js
+// @grant        none
+// ==/UserScript==
--- HEUpgrade.user.js
+++ HEUpgrade.user.js
@@ -5,6 +5,8 @@
 // @description  Upgrading all things which are added to the Que
 // @author       You
 // @match        https://legacy.hackerexperience.com/hardware*
+// @updateURL    https://gitcdn.xyz/repo/Logfro/HEUpgrade/master/HEUpgrade.meta.js
+// @downloadURL  https://gitcdn.xyz/repo/Logfro/HEUpgrade/master/HEUpgrade.user.js
 // @grant        none
 // ==/UserScript==
 
